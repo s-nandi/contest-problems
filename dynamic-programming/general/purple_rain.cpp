b5b96f83b610da306a256b45f593f77297490afd
s-nandi <saujas.nandi@gmail.com>
2017-12-07 20:24:18 -0500

    Update purple_rain.cpp

--- dynamic-programming/general/purple_rain.cpp
+++ dynamic-programming/general/purple_rain.cpp
@@ -1,4 +1,4 @@
-//dynamic programming, kadane's algorithm
+//dynamic programming(kadane's algorithm)
 //https://open.kattis.com/problems/purplerain (2017 Mid-Atlantic Regional)
 
 #include <iostream>
