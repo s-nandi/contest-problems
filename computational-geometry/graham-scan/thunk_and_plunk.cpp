175265859064ecf608c65456180aaf66f2d5199b
s-nandi <saujas.nandi@gmail.com>
2017-12-10 19:07:22 -0500

    Update thunk_and_plunk.cpp

--- computational-geometry/graham-scan/thunk_and_plunk.cpp
+++ computational-geometry/graham-scan/thunk_and_plunk.cpp
@@ -1,4 +1,4 @@
-//graham scan, vector calculations: addition/norm/perpendicular, polygon containment
+//graham scan, trigonometry, polygon containment
 //https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3958
 //2011 Mid-Atlantic Regional
 
