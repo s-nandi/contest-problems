cd7ce2c4aa0f8208102b721f3fc7a83b64767e74
s-nandi <saujas.nandi@gmail.com>
2017-12-28 20:59:35 +0100

    Create python_indentation.cpp

---
+++ dynamic-programming/general/python_indentation.cpp
@@ -0,0 +1,68 @@
+//dynamic programming, prefix sums
+//http://codeforces.com/contest/909/problem/C
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define MOD 1000000007
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector <char> com(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        char c;
+        cin>>com[i];
+    }
+
+    int dp[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            dp[i][j] = 0;
+        }
+    }
+
+    dp[0][0] = 1;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (com[i - 1] == 's')
+        {
+            for (int j = n - 1; j >= 0; j--)
+            {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % MOD;
+
+                if (j + 1 < n)
+                {
+                    dp[i][j] = (dp[i][j] + dp[i][j + 1]) % MOD;
+                }
+            }
+        }
+
+        else
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % MOD;
+            }
+        }
+    }
+
+    int sol = 0;
+    for (int j = 0; j < n; j++)
+    {
+        sol = (sol + dp[n - 1][j]) % MOD;
+    }
+    cout<<sol<<'\n';
+
+    return 0;
+}
