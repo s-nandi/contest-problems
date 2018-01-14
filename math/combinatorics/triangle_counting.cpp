fdb77439cb4e458b85490d3fd1fb07b404db065d
s-nandi <saujas.nandi@gmail.com>
2018-01-13 20:25:06 -0500

    Create triangle_counting.cpp

---
+++ math/combinatorics/triangle_counting.cpp
@@ -0,0 +1,32 @@
+//combinatorics (dp)
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=2396
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define MAXN 1000002
+
+int main()
+{
+    vector <ll> newTriangles(MAXN);
+    vector <ll> dp(MAXN);
+
+    for (int i = 3; i < MAXN; i++)
+    {
+        newTriangles[i] = newTriangles[i - 2] + i - 3;
+        dp[i] = newTriangles[i] + dp[i - 1];
+    }
+
+    int n;
+    while(cin >> n)
+    {
+        if (n < 3) break;
+
+        cout<<dp[n]<<'\n';
+    }
+
+    return 0;
+}
