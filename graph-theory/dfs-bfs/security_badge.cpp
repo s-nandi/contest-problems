3d449ac2667ca81d99a379d8832a41aed1da34fd
s-nandi <saujas.nandi@gmail.com>
2018-01-17 23:41:06 -0500

    Update security_badge.cpp

--- graph-theory/dfs-bfs/security_badge.cpp
+++ graph-theory/dfs-bfs/security_badge.cpp
@@ -1,54 +1,62 @@
 //dfs traversal, interval decomposition
 //https://open.kattis.com/problems/securitybadge (2017 Mid-Atlantic Regional)
 
-#include <set>
 #include <iostream>
-#include <map>
 #include <vector>
-#include <utility>
 #include <algorithm>
 
 using namespace std;
 
-#define ii pair<int, int>
-
-vector <vector<int> > graph;
-map <ii, ii> allowed;
-
-bool dfs(vector <int> &visited, int curr, ii interval, int end)
+struct interval
 {
+    int left, right;
+    bool contains (interval o)
+    {
+        return left <= o.left and o.right <= right;
+    }
+};
+
+struct edge
+{
+    int to;
+    interval valid;
+};
+
+vector <vector<edge>> graph;
+vector <int> visited;
+int start, destination;
+
+bool dfs(int curr, interval attempt)
+{
+    if (visited[curr]) return false;
     visited[curr] = 1;
 
-    if (curr == end)
-    {
-        return true;
-    }
-    bool foundEnd = false;
-    for (int neighbor: graph[curr])
-    {
-        ii requiredRange = allowed[ {curr, neighbor}];
+    if (curr == destination) return true;
 
-        if (!visited[neighbor] and requiredRange.first <= interval.first and requiredRange.second >= interval.second)
+    for (edge e: graph[curr])
+    {
+        int neighbor = e.to;
+        interval allowed = e.valid;
+        if (!visited[neighbor] and allowed.contains(attempt))
         {
-            foundEnd |= dfs(visited, neighbor, interval, end);
+            bool res = dfs(neighbor, attempt);
+            if (res) return true;
         }
     }
-    return foundEnd;
+    return false;
 }
-
 
 int main()
 {
     int n, m, b;
     cin>>n>>m>>b;
-
-    int start, end;
-    cin>>start>>end;
+    cin>>start>>destination;
     --start;
-    --end;
+    --destination;
 
     graph.resize(n);
-    vector <int> changes;
+    visited.resize(n);
+    vector <int> endpoints;
 
     for (int i = 0; i < m; i++)
     {
@@ -56,57 +64,36 @@ int main()
         cin>>a>>b>>l>>r;
         --a;
         --b;
-        graph[a].push_back(b);
 
-        changes.push_back(l);
-        changes.push_back(r);
-
-        allowed[ {a, b}] = {l, r};
+        graph[a].push_back({b, {l, r}});
+        endpoints.push_back(l);
+        endpoints.push_back(r);
     }
+    sort(endpoints.begin(), endpoints.end());
+    endpoints.erase(unique(endpoints.begin(), endpoints.end()), endpoints.end());
 
-    sort(changes.begin(), changes.end());
+    vector <interval> attempts;
+
+    for (int i = 0; i < endpoints.size(); i++)
+    {
+        attempts.push_back({endpoints[i], endpoints[i]});
+        if (i + 1 < endpoints.size() and endpoints[i] + 1 <= endpoints[i + 1] - 1)
+        {
+            attempts.push_back({endpoints[i] + 1, endpoints[i + 1] - 1});
+        }
+    }
 
     int res = 0;
-    bool prevReached = false;
-    set <int> added;
-
-    for (int i = 0; i < changes.size() - 1; i++)
+    for (interval attempt: attempts)
     {
-        vector <int> visited(graph.size(), 0);
-        ii attemptedInterval = {changes[i], changes[i + 1]};
-        bool reached = dfs(visited, start, attemptedInterval, end);
-        if (reached)
+        fill(visited.begin(), visited.end(), 0);
+        if (dfs(start, attempt))
         {
-            added.insert(i);
-            added.insert(i+1);
-
-            res += changes[i+1] - changes[i];
-            if (!prevReached)
-            {
-                res++;
-            }
-            prevReached = true;
-        }
-        else
-        {
-            prevReached = false;
+            res += attempt.right - attempt.left + 1;
         }
     }
 
-    for (int i = 0; i < changes.size(); i++)
-    {
-        if (added.count(i) == 0)
-        {
-            ii attemptedInterval = {changes[i], changes[i]};
-            vector <int> visited(graph.size(), 0);
-            bool reached = dfs(visited,  start, attemptedInterval, end);
-            if (reached)
-            {
-                res++;
-            }
-        }
-    }
+    cout <<res<<'\n';
 
-    cout << res <<'\n';
     return 0;
 }
