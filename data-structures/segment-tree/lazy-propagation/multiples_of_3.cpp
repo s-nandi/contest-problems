0b73d9ca19d42339eb9eff7b082157da9b1f691d
s-nandi <saujas.nandi@gmail.com>
2018-01-26 03:01:02 -0500

    Update multiples_of_3.cpp

--- data-structures/segment-tree/lazy-propagation/multiples_of_3.cpp
+++ data-structures/segment-tree/lazy-propagation/multiples_of_3.cpp
@@ -6,17 +6,36 @@
 
 using namespace std;
 
+struct node
+{
+    int counts[3];
+
+    node lazyUpdate(int v)
+    {
+        node res;
+        for (int i = 0; i < 3; i++)  res.counts[(i + v) % 3] = counts[i];
+        return res;
+    }
+
+    node combine(node o)
+    {
+        node res;
+        for (int i = 0; i < 3; i++) { res.counts[i] = counts[i] + o.counts[i]; }
+        return res;
+    }
+};
+
 struct segmentTree
 {
     int sz, h = 0;
-    vector <vector<int>> elements;
+    vector <node> elements;
     vector <int> lazy;
 
     segmentTree(int s)
     {
         sz = s;
         while (1 << h < sz) h++;
-        elements.resize(2 * sz, {1, 0, 0});
+        elements.resize(2 * sz, {{1, 0, 0}});
         lazy.resize(sz);
     }
 
@@ -24,17 +43,13 @@ struct segmentTree
     {
         for (int i = sz - 1; i >= 1; i--)
         {
-            elements[i][0] = elements[i << 1][0] + elements[i << 1 | 1][0];
+            elements[i] = elements[i << 1].combine(elements[i << 1 | 1]);
         }
     }
 
     void apply(int p, int v)
     {
-        vector <int> save = elements[p];
-        for (int i = 0; i < 3; i++)
-        {
-            elements[p][(i + v) % 3] = save[i];
-        }
+        elements[p] = elements[p].lazyUpdate(v);
         if (p < sz)
         {
             lazy[p] = (lazy[p] + v) % 3;
@@ -57,11 +72,12 @@ struct segmentTree
 
     void siftUp(int p)
     {
-        while (p >= 2)
+        p >>= 1;
+        while (p >= 1)
         {
             for (int i = 0; i < 3; i++)
             {
-                elements[p >> 1][(i + lazy[p >> 1]) % 3] = elements[p][i] + elements[p ^ 1][i];
+                elements[p] = (elements[p << 1].combine(elements[p << 1 | 1])).lazyUpdate(lazy[p]);
             }
             p >>= 1;
         }
@@ -100,23 +116,23 @@ struct segmentTree
         siftDown(l);
         siftDown(r);
 
-        int acc = 0;
+        node acc = {{0, 0, 0}};
         while (l <= r)
         {
             if (l & 1)
             {
-                acc += elements[l][0];
+                acc = acc.combine(elements[l]);
                 l++;
             }
             if (!(r & 1))
             {
-                acc += elements[r][0];
+                acc = acc.combine(elements[r]);
                 r--;
             }
             l >>= 1;
             r >>= 1;
         }
-        return acc;
+        return acc.counts[0];
     }
 };
 
