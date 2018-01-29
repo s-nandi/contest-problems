b9e16a2b6927a9a90416aab1b9071cc9110ca581
s-nandi <saujas.nandi@gmail.com>
2018-01-29 18:49:11 -0500

    Update hidden_password.cpp

--- string-algorithms/strings-with-data-structures/suffix-array/hidden_password.cpp
+++ string-algorithms/strings-with-data-structures/suffix-array/hidden_password.cpp
@@ -23,34 +23,38 @@ struct suffixArray
     vector <vector<int>> table;
     vector <element> suffix;
     int sz, h = 0;
-    int minPrefix;
 
     suffixArray(string s)
     {
         sz = s.length();
         while (1 << h < sz) h++;
 
-        table.resize(sz, vector<int>(h));
+        table.resize(sz, vector<int>(h + 1));
         suffix.resize(sz);
 
         for (int i = 0; i < sz; i++)
         {
             table[i][0] = s[i] - 'a';
         }
-        for (int k = 1, len = 1; k < h; k++, len <<= 1)
+        for (int k = 1; k <= h; k++)
         {
+            int len = 1 << (k - 1);
             for (int i = 0; i < sz; i++)
             {
                 suffix[i] = {{table[i][k - 1], table[(i + len) % sz][k - 1]}, i};
             }
             sort(suffix.begin(), suffix.end());
 
-            minPrefix = suffix[0].index;
             for (int i = 0; i < sz; i++)
             {
                 table[suffix[i].index][k] = i > 0 and suffix[i].ranking == suffix[i - 1].ranking ? table[suffix[i - 1].index][k] : i;
             }
         }
+    }
+
+    int minSuffix()
+    {
+        return suffix[0].index;
     }
 };
 
@@ -72,7 +76,7 @@ int main()
 
         suffixArray sa = suffixArray(s);
 
-        cout<<sa.minPrefix<<endl;
+        cout<<sa.minSuffix()<<endl;
     }
 
     return 0;
