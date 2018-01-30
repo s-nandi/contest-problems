c98281d9e8105a175aa60cc3b453c1415d30ca08
s-nandi <saujas.nandi@gmail.com>
2018-01-30 00:01:18 -0500

    Update hidden_password.cpp

--- string-algorithms/strings-with-data-structures/suffix-array/hidden_password.cpp
+++ string-algorithms/strings-with-data-structures/suffix-array/hidden_password.cpp
@@ -8,8 +8,6 @@
 
 using namespace std;
 
-const int alpha = 26;
-
 struct element
 {
     pair <int, int> ranking;
@@ -20,39 +18,18 @@ struct element
     }
 };
 
-vector <vector <element>> bucket;
-void countingSort(vector <element> &v, bool byFirst)
-{
-    for (int i = 0; i < v.size(); i++)
-    {
-        bucket[byFirst ? v[i].ranking.first : v[i].ranking.second].push_back(v[i]);
-    }
-    for (int i = 0, pos = 0; i < bucket.size(); i++)
-    {
-        for (auto &elem: bucket[i]) v[pos++] = elem;
-        bucket[i].clear();
-    }
-}
-
-void radixSort(vector <element> &v)
-{
-    countingSort(v, false);
-    countingSort(v, true);
-}
-
 struct suffixArray
 {
     vector <vector<int>> table;
     vector <element> suffix;
-    int sz, h = 0, nb;
+    int sz, h = 0;
 
     suffixArray(string s)
     {
         sz = s.length();
         while (1 << h < sz) h++;
-        nb = max(sz, alpha);
 
-        table.resize(sz, vector<int>(h + 1)), suffix.resize(sz), bucket.resize(nb);
+        table.resize(sz, vector<int>(h + 1)), suffix.resize(sz);
 
         for (int i = 0; i < sz; i++)
         {
@@ -65,7 +42,7 @@ struct suffixArray
             {
                 suffix[i] = {{table[i][k - 1], table[(i + len) % sz][k - 1]}, i};
             }
-            radixSort(suffix);
+            sort(suffix.begin(), suffix.end());
             for (int i = 0; i < sz; i++)
             {
                 table[suffix[i].index][k] = i > 0 and suffix[i].ranking == suffix[i - 1].ranking ? table[suffix[i - 1].index][k] : i;
