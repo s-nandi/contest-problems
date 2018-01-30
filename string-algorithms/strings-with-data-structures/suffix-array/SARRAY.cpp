b3b493dc7bfdf40748e3071a5c8c3f49612b09f3
s-nandi <saujas.nandi@gmail.com>
2018-01-30 00:34:31 -0500

    Update SARRAY.cpp

--- string-algorithms/strings-with-data-structures/suffix-array/SARRAY.cpp
+++ string-algorithms/strings-with-data-structures/suffix-array/SARRAY.cpp
@@ -1,64 +1,85 @@
-//suffix array
+//suffix array (radix sort, inverse SA, space reduction)
 //http://www.spoj.com/problems/SARRAY/
 
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int mapping(char c)
-{
-    return int(c);
-}
+const int alpha = 128;
 
 struct element
 {
     pair <int, int> ranking;
     int index;
-    bool operator < (element o) const
-    {
-        return ranking != o.ranking ? ranking < o.ranking : index < o.index;
-    }
 };
+
+vector <vector <element>> bucket;
+void countingSort(vector <element> &v, bool byFirst)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        bucket[byFirst ? v[i].ranking.first + 1 : v[i].ranking.second + 1].push_back(v[i]);
+    }
+    for (int i = 0, pos = 0; i < bucket.size(); i++)
+    {
+        for (auto &elem: bucket[i]) v[pos++] = elem;
+        bucket[i].clear();
+    }
+}
+
+void radixSort(vector <element> &v)
+{
+    countingSort(v, false);
+    countingSort(v, true);
+}
 
 struct suffixArray
 {
-    vector <vector<int>> table;
+    vector <int> table, inverseTable;
     vector <element> suffix;
-    int sz, h = 0;
+    int sz, h = 0, nb;
 
     suffixArray(string s)
     {
         sz = s.length();
         while (1 << h < sz) h++;
+        nb = max(sz, alpha);
 
-        table.resize(sz, vector<int>(h + 1));
-        suffix.resize(sz);
+        table.resize(sz), suffix.resize(sz), inverseTable.resize(sz);
+        bucket.clear(), bucket.resize(nb + 1);
 
         for (int i = 0; i < sz; i++)
         {
-            table[i][0] = mapping(s[i]);
+            table[i] = int(s[i]);
         }
         for (int k = 1; k <= h; k++)
         {
             int len = 1 << (k - 1);
             for (int i = 0; i < sz; i++)
             {
-                suffix[i] = {{table[i][k - 1], i + len < sz ? table[i + len][k - 1] : -1}, i};
+                suffix[i] = {{table[i], i + len < sz ? table[i + len] : -1}, i};
             }
-            sort(suffix.begin(), suffix.end());
-
+            radixSort(suffix);
             for (int i = 0; i < sz; i++)
             {
-                table[suffix[i].index][k] = i > 0 and suffix[i].ranking == suffix[i - 1].ranking ? table[suffix[i - 1].index][k] : i;
+                table[suffix[i].index] = i > 0 and suffix[i].ranking == suffix[i - 1].ranking ? table[suffix[i - 1].index] : i;
             }
+        }
+        for (int i = 0; i < sz; i++)
+        {
+            inverseTable[(*this)[i]] = i;
         }
     }
 
     int operator [] (int i)
     {
-        return table[i][h];
+        return table[i];
+    }
+
+    int inverse(int i)
+    {
+        return inverseTable[i];
     }
 };
 
@@ -71,15 +92,10 @@ int main()
     cin>>s;
 
     suffixArray sa(s);
-    vector <int> solution(s.length());
 
     for (int i = 0; i < s.length(); i++)
     {
-        solution[sa[i]] = i;
-    }
-    for (int i: solution)
-    {
-        cout<<i<<'\n';
+        cout<<sa.inverse(i)<<'\n';
     }
 
     return 0;
