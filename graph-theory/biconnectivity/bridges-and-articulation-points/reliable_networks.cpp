b48029e0a91f8a20989c10a62905008a0fe5598b
s-nandi <saujas.nandi@gmail.com>
2018-02-05 00:44:57 -0500

    Create reliable_networks.cpp

---
+++ graph-theory/biconnectivity/bridges-and-articulation-points/reliable_networks.cpp
@@ -0,0 +1,114 @@
+//bridge detection (edge biconnectivity), bitmasks
+//http://www.spoj.com/problems/RELINETS/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+#define INF 1231231234
+
+struct edge
+{
+    int to, id;
+};
+
+typedef vector<vector<edge>> graph;
+
+struct node
+{
+    int depth = -1;
+    int lowlink = -1;
+};
+
+int numBridges, numReached;
+bool dfs(graph &g, int curr, int prev, int prevEdge, vector <node> &nodes, int &mask)
+{
+    if (nodes[curr].depth != -1)
+    {
+        nodes[prev].lowlink = min(nodes[prev].lowlink, nodes[curr].depth);
+        return false;
+    }
+
+    nodes[curr].depth = prev != -1 ? nodes[prev].depth + 1 : 0;
+    nodes[curr].lowlink = nodes[curr].depth;
+
+    for (edge &e: g[curr]) if (e.id != prevEdge and mask & (1 << e.id))
+    {
+        if (dfs(g, e.to, curr, e.id, nodes, mask))
+        {
+            nodes[curr].lowlink = min(nodes[curr].lowlink, nodes[e.to].lowlink);
+            if (nodes[e.to].lowlink == nodes[e.to].depth)
+            {
+                numBridges++;
+            }
+        }
+    }
+    numReached++;
+    return true;
+}
+
+bool isEdgeBiconnected(graph &g, int &mask)
+{
+    vector <node> nodes(g.size());
+
+    numBridges = 0, numReached = 0;
+    dfs(g, 0, -1, -1, nodes, mask);
+
+    return numBridges == 0 and numReached == g.size();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    for (int test = 1;; test++)
+    {
+        int n, m;
+        cin>>n>>m;
+        if (n == 0 and m == 0) break;
+
+        graph g(n);
+        vector <ll> costs(m);
+
+        for (int i = 0; i < m; i++)
+        {
+            int a, b;
+            cin>>a>>b>>costs[i];
+            --a; --b;
+            g[a].push_back({b, i});
+            g[b].push_back({a, i});
+        }
+
+        ll bestCost = INF;
+        for (int mask = 0; mask < (1 << m); mask++)
+        {
+            ll currCost = 0;
+            for (int i = 0; i < m; i++)
+            {
+                if (mask & (1 << i))
+                {
+                    currCost += costs[i];
+                }
+            }
+
+            if (isEdgeBiconnected(g, mask))
+            {
+                bestCost = min(bestCost, currCost);
+            }
+        }
+
+        if (bestCost == INF)
+        {
+            cout<<"There is no reliable net possible for test case "<<test<<"."<<'\n';
+        }
+        else
+        {
+            cout<<"The minimal cost for test case "<<test<<" is "<<bestCost<<"."<<'\n';
+        }
+    }
+
+    return 0;
+}
