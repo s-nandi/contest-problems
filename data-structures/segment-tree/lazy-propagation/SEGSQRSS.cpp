d42fc7f3cdc3484fb332771d28faaaaf54fb0caf
s-nandi <saujas.nandi@gmail.com>
2018-03-14 19:49:54 -0400

    Update SEGSQRSS.cpp

--- data-structures/segment-tree/lazy-propagation/SEGSQRSS.cpp
+++ data-structures/segment-tree/lazy-propagation/SEGSQRSS.cpp
@@ -1,4 +1,4 @@
-//segment tree, lazy propagation (order/length dependent), sum of squares queries, range increment update, range assignment update
+//segment tree, lazy propagation (iterative), sum of squares queries, range increment update, range assignment update
 //http://www.spoj.com/problems/SEGSQRSS/
 
 #include <iostream>
@@ -7,63 +7,70 @@
 using namespace std;
 
 #define ll long long
-#define INF 1231231234
 
 struct node
 {
-    ll sum, sumSquares;
+    ll sum, sumSquares, lazy[2]; bool isLazy[2];
+    int len = 1;
 
-    node lazyUpdate(int v, int len, int t)
-    {
-        if (t == 0)
-        {
-            return {(ll) len * v, (ll) len * v * v};
-        }
-        else
-        {
-            return {sum + len * v, sumSquares + v * len + 2 * sum * v};
-        }
-    }
+    node(){sum = 0, sumSquares = 0, lazy[0] = lazy[1] = isLazy[0] = isLazy[1] = false;}
+    node(ll a, ll b, int c) : node() {sum = a, sumSquares = b, len = c;}
+    void init(int i){sum = i, sumSquares = i * i;}
 
     node combine(node o)
     {
-        return {sum + o.sum, sumSquares + o.sumSquares};
+        return {sum + o.sum, sumSquares + o.sumSquares, len + o.len};
+    }
+
+    void processLazy(int t)
+    {
+        if (t == 0) sum = len * lazy[t], sumSquares = len * lazy[t] * lazy[t];
+        else sumSquares += len * lazy[t] + 2 * sum * lazy[t], sum += len * lazy[t];
+    }
+
+    void setLazy(int v, int t)
+    {
+        lazy[t] = t ? lazy[t] + v : v, isLazy[t] = true;
+    }
+
+    void resetLazy(int t)
+    {
+        lazy[t] = 0, isLazy[t] = false;
     }
 };
 
 struct segmentTree
 {
-    int sz, h = 0, NIL = -INF;
+    int sz, h = 0;
     vector <node> elements;
-    vector <int> len, lazy[2];
 
     segmentTree(int s)
     {
-        sz = s;
-        while (1 << h < sz) h++;
-        elements.resize(2 * sz), len.resize(2 * sz, 1), lazy[0].resize(sz, NIL), lazy[1].resize(sz, NIL);
+        sz = s, calcHeight();
+        elements.resize(2 * sz);
     }
-
-    node& operator [] (int i)
-    {
-        return elements[i + sz];
-    }
+    void calcHeight(){while(1 << h < sz) h++;}
+    node& operator [] (int i){return elements[i + sz];}
 
     void build()
     {
         for (int i = sz - 1; i >= 1; i--)
         {
             elements[i] = elements[i << 1].combine(elements[i << 1 | 1]);
-            len[i] = len[i << 1] + len[i << 1 | 1];
         }
     }
 
-    void apply(int p, int v, int type)
+    void pushLazy(int p)
     {
-        elements[p] = elements[p].lazyUpdate(v, len[p], type);
-        if (p < sz)
+        for (int t = 0; t < 2; t++) if (elements[p].isLazy[t])
         {
-            lazy[type][p] = v;
+            elements[p].processLazy(t);
+            if (p < sz)
+            {
+                int v = elements[p].lazy[t];
+                elements[p << 1].setLazy(v, t), elements[p << 1 | 1].setLazy(v, t);
+            }
+            elements[p].resetLazy(t);
         }
     }
 
@@ -71,80 +78,50 @@ struct segmentTree
     {
         for (int u = h; u >= 1; u--)
         {
-            int i = p >> u;
-            for (int t = 0; t < 2; t++)
-            {
-                if (lazy[t][i] != NIL)
-                {
-                    apply(i << 1, lazy[t][i], t);
-                    apply(i << 1 | 1, lazy[t][i], t);
-                    lazy[t][i] = NIL;
-                }
-            }
+            pushLazy(p >> u);
         }
     }
 
     void siftUp(int p)
     {
-        p >>= 1;
-        while (p >= 1)
+        pushLazy(p);
+        while (p >= 2)
         {
-            elements[p] = elements[p << 1].combine(elements[p << 1 | 1]);
-            for (int t = 0; t < 2; t++)
-            {
-                if (lazy[t][p] != NIL)
-                {
-                    elements[p] = elements[p].lazyUpdate(lazy[t][p], len[p], t);
-                }
-            }
+            elements[p >> 1] = elements[p].combine(elements[p ^ 1]);
             p >>= 1;
         }
     }
 
     void modify(int l, int r, int v, int t)
     {
-        l += sz, r += sz;
-        siftDown(l), siftDown(r);
-        int lSave = l, rSave = r;
-        while (l <= r)
+        siftDown(l += sz), siftDown(r += sz);
+        auto lr = make_pair(l, r);
+        for (; l <= r; ++l >>= 1, --r >>= 1)
         {
             if (l & 1)
             {
-                apply(l, v, t);
-                l++;
+                elements[l].setLazy(v, t);
+                pushLazy(l);
             }
             if (!(r & 1))
             {
-                apply(r, v, t);
-                r--;
+                elements[r].setLazy(v, t);
+                pushLazy(r);
             }
-            l >>= 1;
-            r >>= 1;
         }
-        siftUp(lSave), siftUp(rSave);
+        siftUp(lr.first), siftUp(lr.second);
     }
 
     ll query(int l, int r)
     {
-        l += sz, r += sz;
-        siftDown(l), siftDown(r);
-        node acc = {0, 0};
-        while (l <= r)
+        node lacc, racc;
+        siftDown(l += sz), siftDown(r += sz);
+        for(; l <= r; ++l >>= 1, --r >>= 1)
         {
-            if (l & 1)
-            {
-                acc = acc.combine(elements[l]);
-                l++;
-            }
-            if (!(r & 1))
-            {
-                acc = acc.combine(elements[r]);
-                r--;
-            }
-            l >>= 1;
-            r >>= 1;
+            if (l & 1) lacc = lacc.combine(elements[l]);
+            if (!(r & 1)) racc = elements[r].combine(racc);
         }
-        return acc.sumSquares;
+        return lacc.combine(racc).sumSquares;
     }
 };
 
@@ -153,10 +130,10 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int tests;
-    cin>>tests;
+    int T;
+    cin>>T;
 
-    for (int test = 1; test <= tests; test++)
+    for (int test = 1; test <= T; test++)
     {
         cout<<"Case "<<test<<":"<<'\n';
         int n, m;
@@ -167,7 +144,7 @@ int main()
         {
             int num;
             cin>>num;
-            st[i] = {num, num * num};
+            st[i].init(num);
         }
         st.build();
 
