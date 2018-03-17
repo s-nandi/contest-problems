b6007e06dcf2f5c2141e3ce3d0f295f5c3588ffa
s-nandi <saujas.nandi@gmail.com>
2018-03-17 01:59:45 -0400

    Update polygon.cpp

--- computational-geometry/graham-scan/polygon.cpp
+++ computational-geometry/graham-scan/polygon.cpp
@@ -1,4 +1,4 @@
-//graham scan
+//andrew monotone chain
 //http://codeforces.com/problemset/problem/166/B
 
 #include <iostream>
@@ -9,60 +9,49 @@ using namespace std;
 
 #define ll long long
 
+typedef int ptsT;
+typedef ll ptlT;
+
 struct pt
 {
-    int x, y;
+    ptsT x, y;
 
-    bool operator < (const pt &o) const
-    {
-        return make_pair(y, x) < make_pair(o.y, o.x);
-    }
+    bool operator < (const pt &o) const {return make_pair(y, x) < make_pair(o.y, o.x);}
+    bool operator == (const pt &o) const {return x == o.x and y == o.y;}
 
-    bool operator == (const pt &o) const
-    {
-        return x == o.x and y == o.y;
-    }
+    pt operator - (const pt &o){return {x - o.x, y - o.y};}
 
-    ll distSq(pt &o)
-    {
-        return (ll) (x - o.x) * (x - o.x) + (ll) (y - o.y) * (y - o.y);
-    }
+    ptlT operator ^ (const pt &o){return (ptlT) x * o.y - (ptlT) y * o.x;}
+    ptlT operator * (const pt &o){return (ptlT) x * o.x + (ptlT) y * o.y;}
+
+    ptlT abs(){return *this * *this;}
+    ptlT dist2(const pt &o){return (*this - o).abs();}
 };
 
-int sgn(ll i)
-{
-    return (i > 0) - (i < 0);
-}
-
+int sgn(ll i) {return (i > 0) - (i < 0);}
 int orientation(pt &a, pt &b, pt &c)
 {
-    return sgn((ll) (b.y - a.y) * (c.x - b.x) - (ll) (b.x - a.x) * (c.y - b.y));
+    return sgn((c - b) ^ (b - a)); //cw: 1, ccw: -1, col: 0
 }
 
-pt pivot;
-bool angleOrder(pt &a, pt &b)
+typedef vector<pt> polygon;
+
+polygon andrewMonotoneChain(polygon &points)
 {
-    int orient = orientation(pivot, a, b);
-    if (orient == 0) return pivot.distSq(a) < pivot.distSq(b);
-    else return orient == 1;
-}
-
-vector <pt> grahamScan(vector <pt> &points)
-{
-    swap(*points.begin(), *min_element(points.begin(), points.end()));
-    pivot = points[0];
-    sort(points.begin() + 1, points.end(), angleOrder);
-
-    int curr = points.size() - 1;
-    while (orientation(points[0], points[curr], points[curr - 1]) == 0) curr--;
-    reverse(points.begin() + curr, points.end());
-
-    vector <pt> hull = {points[0], points[1], points[2]};
-    for (int i = 3; i < points.size(); i++)
+    sort(points.begin(), points.end());
+    polygon hull;
+    for (int i = 0; i < 2; i++)
     {
-        while (orientation(hull.rbegin()[1], hull.rbegin()[0], points[i]) == -1) hull.pop_back();
-        hull.push_back(points[i]);
+        int s = hull.size();
+        for (pt &p: points)
+        {
+            while(hull.size() >= s + 2 and orientation(hull.rbegin()[1], hull.rbegin()[0], p) == -1) hull.pop_back();
+            hull.push_back(p);
+        }
+        hull.pop_back();
+        reverse(points.begin(), points.end());
     }
+    if (hull.size() == 2 and hull[0] == hull[1]) hull.pop_back();
     return hull;
 }
 
@@ -73,22 +62,22 @@ int main()
 
     int n, m;
     cin>>n;
-    vector <pt> a(n);
+    polygon a(n);
     for (int i = 0; i < n; i++)
     {
         cin>>a[i].x>>a[i].y;
     }
-    a = grahamScan(a);
+    a = andrewMonotoneChain(a);
 
     cin>>m;
-    vector <pt> b(m);
+    polygon b(m);
     for (int i = 0; i < m; i++)
     {
         cin>>b[i].x>>b[i].y;
     }
 
     b.insert(b.end(), a.begin(), a.end());
-    vector <pt> hull = grahamScan(b);
+    polygon hull = andrewMonotoneChain(b);
 
     if (a == hull) cout<<"YES"<<'\n';
     else cout<<"NO"<<'\n';
