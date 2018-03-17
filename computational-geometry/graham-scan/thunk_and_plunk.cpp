76be5cac2cd5a3b83ad88ae9a8b2de10877f924e
s-nandi <saujas.nandi@gmail.com>
2018-03-17 15:11:58 -0400

    Update thunk_and_plunk.cpp

--- computational-geometry/graham-scan/thunk_and_plunk.cpp
+++ computational-geometry/graham-scan/thunk_and_plunk.cpp
@@ -1,266 +1,109 @@
-//graham scan, trigonometry, convex polygon containment
-//https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3958
+//andrew monotone chain, vector geometry, convex polygon containment
+//https://icpcarchive.ecs.baylor.edu/index.php?option=onlinejudge&page=show_problem&problem=3958
 //2011 Mid-Atlantic Regional
 
-#include <stack>
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <cmath>
 
 using namespace std;
 
+#define ll long long
+
+typedef double ptsT;
+typedef long double ptlT;
+
+const double EPS = 1e-9;
 
 struct pt
 {
-    double x, y;
+    ptsT x, y;
 
-    bool operator < (pt b) const
-    {
-        if (y < b.y)
-        {
-            return true;
-        }
-        else if(y == b.y)
-        {
-            if (x < b.x)
-            {
-                return true;
-            }
+    bool operator < (const pt &o) const {return make_pair(y, x) < make_pair(o.y, o.x);}
+    bool operator == (const pt &o) const {return abs(x - o.x) < EPS and abs(y - o.y) < EPS;}
 
-        }
-        return false;
-    }
+    pt operator + (const pt &o){return {x + o.x, y + o.y};}
+    pt operator - (const pt &o){return {x - o.x, y - o.y};}
+    pt operator * (ptsT scale){return {x * scale, y * scale};}
+    pt operator / (ptsT scale){return {x / scale, y / scale};}
+
+    ptlT operator ^ (const pt &o){return (ptlT) x * o.y - (ptlT) y * o.x;}
+    ptlT operator * (const pt &o){return (ptlT) x * o.x + (ptlT) y * o.y;}
+
+    ptlT norm2(){return *this * *this;}
+    ptlT dist2(const pt &o){return (*this - o).norm2();}
+    pt normalize(){return *this / sqrt(norm2());}
+
+    pt perpccw(){return {-y, x};}
+    pt perpcw(){return perpccw() * -1;}
 };
 
-bool onSegment(pt p, pt q, pt r)
+int sgn(ptlT i) {return (i > -EPS) - (i < EPS);}
+int orientation(pt &o, pt &a, pt &b) {return sgn((b - o) ^ (a - o));} //cw: 1, ccw: -1, col: 0
+
+typedef vector<pt> polygon;
+int prev(int i, int n){return i == 0 ? n - 1 : i - 1;}
+int next(int i, int n){return i == n - 1 ? 0 : i + 1;};
+
+polygon andrewMonotoneChain(vector <pt> &points)
 {
-    if (q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) &&
-            q.y <= max(p.y, r.y) && q.y >= min(p.y, r.y))
-        return true;
-    return false;
-}
-
-int orientation(pt a, pt b, pt c)
-{
-    double  val1 = (double) (b.y - a.y) * (c.x - b.x);
-    double val2 = (double) (b.x - a.x) * (c.y - b.y);
-
-    if (val1 < val2)
+    sort(points.begin(), points.end());
+    points.erase(unique(points.begin(), points.end()), points.end());
+    polygon hull;
+    for (int i = 0; i < 2; i++)
     {
-        return -1; //counterclockwise
-    }
-    else if (val1 > val2)
-    {
-        return 1; //clockwise
-    }
-    else
-    {
-        return 0; //collinear
-    }
-}
-
-bool onLine(pt a, pt b, pt c) //true if b is on ac
-{
-    if (orientation(a, b, c) != 0)
-    {
-        return false;
-    }
-    return onSegment(a, b, c);
-
-}
-
-bool inTriangle (pt a, pt b, pt c, pt p)
-{
-    int orientAB = orientation(a, b, p);
-    int orientBC = orientation(b, c, p);
-    int orientCA = orientation(c, a, p);
-    if (orientAB == 0 and onLine(a, p, b))
-    {
-        return true;
-    }
-    if (orientBC == 0 and onLine(b, p, c))
-    {
-        return true;
-    }
-    if (orientCA == 0 and onLine(c, p, a))
-    {
-        return true;
-
-    }
-
-    if (orientAB == orientBC and orientAB == orientCA)
-    {
-        return true;
-    }
-
-    return false;
-}
-
-vector <pt> grahamScan(vector <pt> points) //creates clockwise convex polygon
-{
-    pt bottom = points[0];
-
-    for (int i = 1; i < points.size(); i++)
-    {
-        if (points[i].y == bottom.y)
+        int s = hull.size();
+        for (pt &p: points)
         {
-            if (points[i].x < bottom.x)
-            {
-                bottom = points[i];
-            }
+            while(hull.size() >= s + 2 and orientation(hull.rbegin()[1], hull.rbegin()[0], p) != 1) hull.pop_back();
+            hull.push_back(p);
         }
-        else if(points[i].y < bottom.y)
-        {
-            bottom = points[i];
-        }
+        hull.pop_back();
+        reverse(points.begin(), points.end());
     }
-
-    sort(points.begin(), points.end(),
-         [bottom](pt a, pt b)
-    {
-        if (a.x == bottom.x and a.y == bottom.y)
-        {
-            return true;
-        }
-        if (b.x == bottom.x and b.y == bottom.y)
-        {
-            return false;
-        }
-        int orient = orientation(bottom, a, b);
-        if (orient == 0)
-        {
-            double dista = abs((double) a.x - bottom.x);
-            double distb = abs((double) b.x - bottom.x);
-
-            if (dista == distb)
-            {
-                return b < a;
-            }
-            return dista > distb;
-        }
-        return orient == 1;
-    });
-
-    bool includeNext = true;
-
-
-    vector <pt> vPoints = {points[0]};
-    vector <pt> skipped;
-    for (int i = 1; i < points.size(); i++)
-    {
-        if (includeNext)
-        {
-            vPoints.push_back(points[i]);
-        }
-        else
-        {
-            skipped.push_back(points[i]);
-        }
-
-        includeNext = true;
-        if(orientation(points[0], points[i], points[i + 1]) == 0)
-        {
-            includeNext = false;
-        }
-    }
-
-    vector <pt> hull;
-    hull.push_back(vPoints[0]);
-    hull.push_back(vPoints[1]);
-    hull.push_back(vPoints[2]);
-
-    for (int i = 3; i < vPoints.size(); i++)
-    {
-        while (orientation(hull[hull.size() - 2], hull[hull.size() - 1], vPoints[i]) != 1) // != 1 if you don't want points on hull, == -1 if include points on hull
-        {
-            hull.pop_back();
-        }
-        hull.push_back(vPoints[i]);
-    }
-
+    if (hull.size() == 2 and hull[0] == hull[1]) hull.pop_back();
     return hull;
 }
 
-double dist(pt a, pt b)
+polygon fixConvexPolygon(polygon &poly)
 {
-    double dx = a.x - b.x;
-    double dy = a.y - b.y;
-    return dx * dx + dy * dy;
-}
-
-pt getVector(pt a, pt b)
-{
-    pt temp;
-    temp.x = b.x - a.x;
-    temp.y = b.y - a.y;
-    return temp;
-}
-
-pt normed(pt v)
-{
-    pt o;
-    o.x = 0.0;
-    o.y = 0.0;
-    double len = dist(o, v);
-    pt res;
-    res.x = v.x / sqrt(len);
-    res.y = v.y / sqrt(len);
-
+    int shift = min_element(poly.begin(), poly.end()) - poly.begin();
+    polygon res(poly.size());
+    for (int i = 0; i < poly.size(); i++)
+    {
+        res[i] = poly[(i + shift) % poly.size()];
+    }
     return res;
 }
 
-pt perp(pt sl)
+int pointInTriangle(polygon &triangle, pt &p) //inside: -1, outside: 1, on: 0
 {
-    pt p;
-    p.x = sl.y;
-    p.y = sl.x * -1;
-    return p;
-}
-
-pt addPt(pt a, pt b)
-{
-    pt result;
-    result.x = a.x + b.x;
-    result.y = a.y + b.y;
-    return result;
-}
-
-
-vector <pt> getRectangle(pt a, pt b)
-{
-    pt vec = getVector(a, b);
-    pt per = perp(vec);
-    pt normedVec = normed(per);
-
-    vector <pt> rectangle(4);
-
-    rectangle[0] = a;
-    rectangle[1] = b;
-    rectangle[2] = addPt(b, normedVec);
-    rectangle[3] = addPt(a, normedVec);
-
-    return rectangle;
-}
-
-bool inHull(vector <pt> hull, pt p)
-{
-    if (hull.size() == 3)
+    int orient[3];
+    for (int i = 0; i < 3; i++)
     {
-        return inTriangle(hull[0], hull[1], hull[2], p);
+        orient[i] = orientation(triangle[i], triangle[next(i, 3)], p);
     }
-    bool isIn;
-    for (int i = 1; i < hull.size() - 1; i++)
-    {
-        isIn = inTriangle(hull[0], hull[i], hull[i+1], p);
-        if (isIn)
-        {
-            return true;
-        }
-    }
-    return false;
+    if (orient[0] == orient[1] and orient[1] == orient[2]) return -1;
+    for (int i = 0; i < 3; i++) if (orient[i] * orient[next(i, 3)] == -1) return 1;
+    return 0;
 }
 
+bool pointInConvexPolygon(polygon &poly, pt &point)
+{
+    auto cmp = [&poly](pt a, pt b){return orientation(poly[0], a, b) == 1;};
+    int pos = upper_bound(poly.begin() + 1, poly.end(), point, cmp) - poly.begin();
+    if (pos == 1 or pos == poly.size()) return false;
+    polygon triangle = {poly[pos - 1], poly[pos], poly[0]};
+    return pointInTriangle(triangle, point) != 1;
+}
+
+polygon makeRectangle(pt a, pt b)
+{
+    pt v = (b - a).perpcw().normalize();
+    polygon rectangle = {a - v, b - v, b + v, a + v};
+    return fixConvexPolygon(rectangle);
+}
 
 int main()
 {
@@ -268,103 +111,48 @@ int main()
     cin.tie(NULL);
 
     int n;
-
     while(cin>>n)
     {
-        if (n == 0)
-        {
-            break;
-        }
+        if (n == 0) break;
 
-        vector <pt> water;
-        vector <pt> land;
-        double x, y;
-        char type;
-
+        vector <pt> water, land;
         for (int i = 0; i < n; i++)
         {
+            double x, y; char type;
             cin>>x>>y>>type;
 
-            pt temp;
-            temp.x = x;
-            temp.y = y;
-            if (type == 'T')
-            {
-                land.push_back(temp);
-            }
-            else
-            {
-                water.push_back(temp);
-            }
+            if (type == 'T') land.push_back({x, y});
+            else water.push_back({x, y});
         }
-
         bool isIsland = false;
         if (water.size() == 2)
         {
+            polygon rect = makeRectangle(water[0], water[1]);
             for (pt p: land)
             {
-                vector <pt> rectOne = getRectangle(water[0], water[1]);
-                vector <pt> rectTwo = getRectangle(water[1], water[0]);
-                if (!inHull(rectOne, p) and !inHull(rectTwo, p))
-                {
-                    isIsland = true;
-                }
+                if (!pointInConvexPolygon(rect, p)) isIsland = true;
             }
-            if (isIsland)
-                cout<<"There must be an island."<<'\n';
-            else
-                cout<<"There might not be an island."<<'\n';
-
-
         }
-
         else
         {
-            vector <pt> waterArea = grahamScan(water);
-
-            stack <pt> conflictPoints;
+            int ws = water.size();
+            polygon waterArea = andrewMonotoneChain(water);
             for (pt p: land)
             {
-                if (inHull(waterArea, p))
-                {
-                    conflictPoints.push(p);
-                }
-            }
-            while(!conflictPoints.empty())
-            {
-                pt curr = conflictPoints.top();
-                conflictPoints.pop();
-                bool safe = false;
-
-                for (int i = 0; i < waterArea.size(); i++)
-                {
-                    pt ptA = waterArea[i];
-                    pt ptB = waterArea[(i + 1) % waterArea.size()];
-
-                    vector <pt> rect = getRectangle(ptA, ptB);
-
-                    if (inHull(rect, curr))
+                if (pointInConvexPolygon(waterArea, p))
+                 {
+                    bool inBoundary = false;
+                    for (int i = 0; i < waterArea.size(); i++)
                     {
-                        safe = true;
+                        polygon rect = makeRectangle(waterArea[i], waterArea[next(i, ws)]);
+                        if (pointInConvexPolygon(rect, p)) inBoundary = true;
                     }
-                }
-
-                if (!safe)
-                {
-                    isIsland = true;
-                }
-
-            }
-
-            if (isIsland)
-            {
-                cout<<"There must be an island."<<'\n';
-            }
-            else
-            {
-                cout<<"There might not be an island."<<'\n';
+                    if (!inBoundary) isIsland = true;
+                 }
             }
         }
+        if (isIsland) cout<<"There must be an island."<<'\n';
+        else cout<<"There might not be an island."<<'\n';
     }
     return 0;
 }
