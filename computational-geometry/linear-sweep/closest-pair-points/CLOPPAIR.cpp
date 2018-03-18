98d30586046194abcbb69cbda83132cf4bfc6715
s-nandi <saujas.nandi@gmail.com>
2018-03-18 04:36:09 -0400

    Update CLOPPAIR.cpp

--- computational-geometry/linear-sweep/closest-pair-points/CLOPPAIR.cpp
+++ computational-geometry/linear-sweep/closest-pair-points/CLOPPAIR.cpp
@@ -1,55 +1,56 @@
+//closest pair of points (line sweep)
+//http://www.spoj.com/problems/CLOPPAIR/
+
 #include <iostream>
 #include <vector>
-#include <set>
 #include <algorithm>
-#include <utility>
+#include <set>
 #include <cmath>
 #include <iomanip>
 
 using namespace std;
 
-#define INF 1231231234
+#define ll long long
+
+typedef int ptsT; //PS: otherwise double
+typedef ll ptlT; //PS: otherwise long double
+
+const ll INF = 1e18;
 
 struct pt
 {
-    int x, y, index;
+    ptsT x, y;
+    int index;
 
-    bool operator < (pt o) const
-    {
-        if (y != o.y) return y < o.y;
-        else return x < o.x;
-    }
+    bool operator < (const pt &o) const {return make_pair(y, x) < make_pair(o.y, o.x);}
 
-    double dist(pt o) const
-    {
-        return sqrt((o.x - x) * (o.x - x) + (o.y - y) * (o.y - y));
-    }
+    pt operator - (const pt &o) const {return {x - o.x, y - o.y};}
+
+    ptlT operator * (const pt &o) const {return (ptlT) x * o.x + (ptlT) y * o.y;}
+
+    ptlT norm2() const {return *this * *this;}
+    ptlT dist2(const pt &o) const {return (*this - o).norm2();}
 };
+
+inline ptlT square(ptsT i) {return (ptlT) i * i;}
 
 pair <pt, pt> closestPairPoints(vector <pt> &points)
 {
-    set <pt> window;
-    sort(points.begin(), points.end(), [](pt a, pt b){ return a.x != b.x ? a.x < b.x : a.y < b.y; });
-    window.insert(points[0]);
+    sort(points.begin(), points.end(), [](pt a, pt b){return make_pair(a.x, a.y) < make_pair(b.x, b.y);});
+    set <pt> window = {points[0]};
 
-    double closest = INF;
-    int leftMost = 0;
+    ptlT closest = INF;
     pair <pt, pt> res;
-
-    for (int i = 1; i < points.size(); i++)
+    for (int i = 1, left = 0; i < points.size(); i++)
     {
-        while(i > leftMost and points[i].x - points[leftMost].x > closest) window.erase(points[leftMost++]);
+        while(i > left and square(points[i].x - points[left].x) > closest) window.erase(points[left++]);
 
-        pt bottomMost = {points[i].x, int(points[i].y - closest + .5), -1};
-        pt topMost = {points[i].x, int(points[i].y + closest), -1};
-        for (auto it = window.lower_bound(bottomMost); it != window.upper_bound(topMost); it++)
+        pt lb = {points[i].x, (ptsT) ceil(points[i].y - sqrt(closest)), -1};
+        pt ub = {points[i].x, (ptsT) floor(points[i].y + sqrt(closest)), -1};
+        for (auto it = window.lower_bound(lb); it != window.upper_bound(ub); it++)
         {
-            double distBetween = it -> dist(points[i]);
-            if (distBetween < closest)
-            {
-                closest = distBetween;
-                res = {*it, points[i]};
-            }
+            auto dist = it -> dist2(points[i]);
+            if (dist < closest) closest = dist, res = {*it, points[i]};
         }
         window.insert(points[i]);
     }
@@ -60,23 +61,21 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    setprecision(6);
 
     int n;
     cin>>n;
 
     vector <pt> points(n);
-
     for (int i = 0; i < n; i++)
     {
         cin>>points[i].x>>points[i].y;
         points[i].index = i;
     }
 
-    auto solution = closestPairPoints(points);
-    pt a = solution.first, b = solution.second;
+    auto sol = closestPairPoints(points);
+    pt a = sol.first, b = sol.second;
     if (a.index > b.index) swap(a, b);
-    cout<<fixed<<a.index<<" "<<b.index<<" "<<a.dist(b)<<'\n';
+    cout<<setprecision(6)<<fixed<<a.index<<" "<<b.index<<" "<<sqrt(a.dist2(b))<<'\n';
 
     return 0;
 }
