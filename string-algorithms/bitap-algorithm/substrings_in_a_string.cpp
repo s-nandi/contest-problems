bf3717fddb1f9e015d3756a24d5a16fc81f25b8f
s-nandi <saujas.nandi@gmail.com>
2018-03-19 04:10:00 -0400

    Update substrings_in_a_string.cpp

--- string-algorithms/bitap-algorithm/substrings_in_a_string.cpp
+++ string-algorithms/bitap-algorithm/substrings_in_a_string.cpp
@@ -1,40 +1,45 @@
-//bitap, modifying character updates, word matching queries
+//bitap (with updates), word matching queries
 //http://codeforces.com/contest/914/problem/F
 
 #include <iostream>
 #include <vector>
+#include <array>
 #include <bitset>
 
 using namespace std;
 
-#define MAXN 100001
+const int MAXN = 100001;
+const int alpha = 26;
 
-string text;
-bitset <MAXN> mask[26];
+typedef array<bitset<MAXN>, alpha> mask;
 
-void buildMask()
+int mapping(char c){return c - 'a';}
+char invmap(int i){return i + 'a';}
+
+mask buildMask(string &text)
 {
+    mask mk;
     for (int i = 0; i < text.length(); i++)
     {
-        mask[text[i] - 'a'][i] = 1;
+        mk[mapping(text[i])][i] = 1;
     }
+    return mk;
 }
 
-void modifyText(int p, char c)
+void modifyText(mask &mk, string &text, int p, char c)
 {
-    mask[text[p] - 'a'][p] = 0;
-    mask[c - 'a'][p] = 1;
+    mk[mapping(text[p])][p] = 0;
+    mk[mapping(c)][p] = 1;
     text[p] = c;
 }
 
-int bitap(string &word, int l, int r)
+int bitapMatch(mask &mk, string &word, int l, int r)
 {
     if (l + word.length() - 1 > r) return 0;
-
     bitset <MAXN> matches = bitset<MAXN>().set();
     for (int i = 0; i < word.length(); i++)
     {
-        matches &= mask[word[i] - 'a'] >> i;
+        matches &= mk[word[i] - 'a'] >> i;
         if (matches.none()) return 0;
     }
     return (matches >> l).count() - (matches >> r - word.length() + 2).count();
@@ -45,12 +50,12 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
+    string text;
     cin>>text;
-    buildMask();
+    auto mk = buildMask(text);
 
     int q;
     cin>>q;
-
     for (int i = 0; i < q; i++)
     {
         int type;
@@ -59,13 +64,13 @@ int main()
         {
             int i; char c;
             cin>>i>>c;
-            modifyText(--i, c);
+            modifyText(mk, text, --i, c);
         }
         else
         {
-            int l, r; string word;
-            cin>>l>>r>>word;
-            cout<<bitap(word, --l, --r)<<'\n';
+            int l, r; string s;
+            cin>>l>>r>>s;
+            cout<<bitapMatch(mk, s, --l, --r)<<'\n';
         }
     }
 
