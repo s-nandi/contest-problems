0545b66ad5c7cee844ee051ee7931457e25648ad
s-nandi <saujas.nandi@gmail.com>
2018-03-19 03:56:07 -0400

    Update NHAY.cpp

--- string-algorithms/kmp-algorithm/NHAY.cpp
+++ string-algorithms/kmp-algorithm/NHAY.cpp
@@ -8,44 +8,26 @@ using namespace std;
 
 vector <int> prefixFunction(string &s)
 {
-    int n = s.length();
-    vector <int> pf(n);
-
-    for (int i = 1; i < n; i++)
+    vector <int> pf(s.length());
+    for (int i = 1; i < s.length(); i++)
     {
         int j = pf[i - 1];
-        while (j > 0 and s[i] != s[j])
-        {
-            j = pf[j - 1];
-        }
+        while (j > 0 and s[i] != s[j]) j = pf[j - 1];
         if (s[i] == s[j]) pf[i] = j + 1;
         else pf[i] = 0;
     }
-
     return pf;
 }
 
-vector <int> kmp(string &text, string &s)
+vector <int> kmpMatch(string &text, string &s, const vector <int> &pfs)
 {
-    auto pf = prefixFunction(s);
-
     vector <int> matches;
-
     for (int i = 0, j = 0; i < text.length(); i++)
     {
-        while (j > 0 and text[i] != s[j])
-        {
-            j = pf[j - 1];
-        }
-        if (text[i] == s[j])
-        {
-            if (++j == s.length())
-            {
-                matches.push_back(i + 1 - s.length());
-            }
-        }
+        while (j > 0 and text[i] != s[j]) j = pfs[j - 1];
+        if (text[i] == s[j] and ++j == s.length())
+            matches.push_back(i + 1 - s.length());
     }
-
     return matches;
 }
 
@@ -58,12 +40,9 @@ int main()
     while (cin>>n)
     {
         string s, t;
-
         cin>>s>>t;
 
-        auto res = kmp(t, s);
-
-        for (int i: res)
+        for (int i: kmpMatch(t, s, prefixFunction(s)))
         {
             cout<<i<<'\n';
         }
