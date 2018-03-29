b924aa48ac07e35ffd8f19b0bc70a67e0a428398
s-nandi <saujas.nandi@gmail.com>
2018-03-29 00:45:33 -0400

    Update CLOPPAIR.cpp

--- computational-geometry/linear-sweep/closest-pair-points/CLOPPAIR.cpp
+++ computational-geometry/linear-sweep/closest-pair-points/CLOPPAIR.cpp
@@ -12,41 +12,42 @@ using namespace std;
 
 #define ll long long
 
-typedef int ptsT; //PS: otherwise double
-typedef ll ptlT; //PS: otherwise long double
+typedef ll ptT;
 
 const ll INF = 1e18;
 
 struct pt
 {
-    ptsT x, y;
+    ptT x, y;
     int index;
 
     bool operator < (const pt &o) const {return make_pair(y, x) < make_pair(o.y, o.x);}
 
     pt operator - (const pt &o) const {return {x - o.x, y - o.y};}
 
-    ptlT operator * (const pt &o) const {return (ptlT) x * o.x + (ptlT) y * o.y;}
+    ptT operator * (const pt &o) const {return (ptT) x * o.x + (ptT) y * o.y;}
 
-    ptlT norm2() const {return *this * *this;}
-    ptlT dist2(const pt &o) const {return (*this - o).norm2();}
+    ptT norm2() const {return *this * *this;}
+    ptT dist2(const pt &o) const {return (*this - o).norm2();}
 };
 
-inline ptlT square(ptsT i) {return (ptlT) i * i;}
+auto cmpByX = [](const pt &a, const pt &b){return make_pair(a.x, a.y) < make_pair(b.x, b.y);};
+
+ptT sq(ptT i) {return i * i;}
 
 pair <pt, pt> closestPairPoints(vector <pt> &points)
 {
-    sort(points.begin(), points.end(), [](pt a, pt b){return make_pair(a.x, a.y) < make_pair(b.x, b.y);});
+    sort(points.begin(), points.end(), cmpByX);
     set <pt> window = {points[0]};
 
-    ptlT closest = INF;
+    ptT closest = INF;
     pair <pt, pt> res;
     for (int i = 1, left = 0; i < points.size(); i++)
     {
-        while(i > left and square(points[i].x - points[left].x) > closest) window.erase(points[left++]);
+        while(i > left and sq(points[i].x - points[left].x) > closest) window.erase(points[left++]);
 
-        pt lb = {points[i].x, (ptsT) ceil(points[i].y - sqrt(closest)), -1};
-        pt ub = {points[i].x, (ptsT) floor(points[i].y + sqrt(closest)), -1};
+        pt lb = {points[i].x, (ptT) ceil(points[i].y - sqrt(closest))};
+        pt ub = {points[i].x, (ptT) floor(points[i].y + sqrt(closest))};
         for (auto it = window.lower_bound(lb); it != window.upper_bound(ub); it++)
         {
             auto dist = it -> dist2(points[i]);
