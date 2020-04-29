63e5c0d7684177ac573df4d0236add470bb7f60b
s-nandi <saujas.nandi@gmail.com>
2020-04-29 05:51:00 -0400

    Update farey_sequence_length.cpp

--- math/number-theory/totient-sieve/farey_sequence_length.cpp
+++ math/number-theory/totient-sieve/farey_sequence_length.cpp
@@ -3,62 +3,36 @@
 // 2015 Greater NY Regional
 
 #include <bits/stdc++.h>
-
 using namespace std;
 
-typedef long long ll;
+#define rep(i,a,b) for(int i=(a);i<(b);++i)
 
 const int MAXN = 10000;
+int phi[MAXN + 5];
+bool comp[MAXN + 5];
 
-vector <int> primes, phi;
-vector <bool> prm;
-void totientSieve(int n)
-{
-    prm.resize(n + 1, true);
-    phi.resize(n + 1);
-    for (int i = 2; i <= n; i++)
-    {
-        if (prm[i])
-        {
-            primes.push_back(i);
-            phi[i] = i - 1;
-        }
-        for (int prime: primes)
-        {
-            if ((ll) i * prime > n)
-                break;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
 
-            prm[i * prime] = false;
-            if (i % prime == 0)
-            {
-                phi[i * prime] = phi[i] * prime;
-                break;
-            }
-            else
-                phi[i * prime] = phi[i] * phi[prime];
-        }
+  phi[1] = 1;
+  rep(i, 2LL, MAXN + 1) if (!comp[i]) {
+    phi[i] = i - 1;
+    rep(j, 2, MAXN / i + 1) {
+      comp[i * j] = true;
+      phi[i * j] = j % i ? phi[i] * phi[j] : i * phi[j];
     }
-}
+  }
+  vector<int> ps(MAXN + 1);
+  ps[0] = 1; // (0 / n) is also contained in a n-length farey sequence
+  for (int i = 1; i <= MAXN; i++)
+    ps[i] = ps[i - 1] + phi[i];
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-
-    totientSieve(MAXN);
-
-    int T;
-    cin >> T;
-
-    vector <int> ps(MAXN + 1);
-    ps[0] = 2; // (0 / n) and (n / n) always contained in a n-length farey sequence
-    for (int i = 1; i <= MAXN; i++)
-        ps[i] = ps[i - 1] + phi[i];
-
-    while (T--)
-    {
-        int test, n;
-        cin >> test >> n;
-        cout << test << " " << ps[n] << '\n';
-    }
+  int T;
+  cin >> T;
+  while (T--) {
+    int tnum, n;
+    cin >> tnum >> n;
+    cout << tnum << " " << ps[n] << '\n';
+  }
 }
